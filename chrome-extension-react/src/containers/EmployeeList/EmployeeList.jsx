588effc4235e36b15c4dab82d50652f12c1ed3cb
akshaj65 <jhonsonrozario13@gmail.com>
2024-05-16 14:11:59 +0530

    Updated employee List and Item

--- chrome-extension-react/src/containers/EmployeeList/EmployeeList.jsx
+++ chrome-extension-react/src/containers/EmployeeList/EmployeeList.jsx
@@ -1,27 +1,46 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.module.css'
-
+import EmployeeItem from '../EmployeeItem/item'
 
 const EmployeeList = ({ data }) => {
+    const [visible, setVisible] = useState(false)
+    const [employee, setEmployee] = useState(false)
+    const handleData = (id) => {
+        if (!id)
+            return
+        const employee = data.find(employee => employee._id === id)
+        setEmployee(employee)
+        setVisible(true)
+    }
     useEffect(() => {
+
         console.log('data changed', data);
-       
+
     }, [data])
 
     return (
-        <div id="eqweq-employeeList">
-            <ul>
-            {data.map((employee) => {
-            return (
-                <li>
-                    <span>{employee.firstName}</span>
-                    {/* underline this highlight this */}
-                    <span>{employee.lastName}</span>
-                </li>
-            )
-        })}
-            </ul>
-        </div>
+        <>
+            {visible && employee
+                && < EmployeeItem employee={employee} />
+            }
+            {!visible
+                && <div id="eqweq-employeeList">
+                    <ul>
+                        {data.map((employee) => {
+                            return (
+                                <li key={employee._id} onClick={() => handleData(employee._id)}>
+                                    <span>{employee.firstName}</span>
+                                    {/* underline this highlight this */}
+                                    <span>{employee.lastName}</span>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            }
+
+        </>
+
     )
 }
 
